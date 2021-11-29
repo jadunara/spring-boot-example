2c3d58686a20bd2ffffe0bcc063060c0654b84ad
DESKTOP-O7HT82C\ktnam <ghost@jadu.pe.kr>
2021-11-29 17:34:30 +0900

    bootstrap, vue init.

--- spring-boot-model/src/main/java/com/example/demo/ui/HomeController.java
+++ spring-boot-model/src/main/java/com/example/demo/ui/HomeController.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.example.demo.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +12,7 @@ public class HomeController {
 		System.out.println("-------------------------");
 		return "home";
 	}
-	@RequestMapping(value = "/index.do")
+	@GetMapping(value = "/index.do")
 	public String index(Model model) {
 		System.out.println("-------------------------");
 		return "index";
